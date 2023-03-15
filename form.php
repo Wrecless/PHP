4f2167bb0e6d2d89688013d61dee74e0aecaabe2
Wrecless <73550513+Wrecless@users.noreply.github.com>
2023-03-15 15:51:17 +0000

    php

--- form.php
+++ form.php
@@ -11,16 +11,22 @@
             <legend>Personal Information</legend>
             <div>
                 <label for="name">Name</label>
-                <input type="text" name="name" id="name">
+                <input type="text" name="name" id="name" required>
             </div>
             <div>
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email">
+                <input type="email" name="email" id="email" required>
             </div>
             <div>
                 <label for="password">Password</label>
-                <input type="password" name="password" id="password">
+                <input type="password" name="password" id="password" required>
             </div>
+	        <div>
+		        <label for="postcode">Postcode</label>
+		        <input type="text" name="postcode" id="postcode" required pattern="[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}"
+		        title="Please enter a valid postcode"
+		        >
+	        </div>
             <div>
                 <label for="telephone">Telephone</label>
                 <input type="tel" name="telephone" id="telephone">
@@ -41,7 +47,8 @@
             </div>
 
             <div>
-                textarea: <textarea name="textarea" id="textarea" placeholder="Content"></textarea>
+                <p>Text area</p>
+	            <textarea name="textarea" id="textarea" placeholder="Content"></textarea>
             </div>
 
             <div>
@@ -68,10 +75,24 @@
                 <label for="green">Green</label>
             </div>
 
-            <div>
-                <input type="submit" value="Submit">
-            </div>
         </fieldset>
+	    <fieldset>
+		    <legend>
+			    Select a country
+		    </legend>
+		    <div>
+			    <label for="country">Country</label>
+			    <select name="country" id="country">
+				    <option value="uk">United Kingdom</option>
+				    <option value="us">United States</option>
+				    <option value="ca">Canada</option>
+			    </select>
+		    </div>
+	    </fieldset>
+
+	    <div>
+		    <input type="submit" value="Submit">
+	    </div>
 
     </form>
 </body>
